4630a545b5dae7dcdb272fc9b55d2f5c920f3a94
故、新 <xingej@163.com>
2020-02-28 15:22:05 +0800

    添加注释说明

--- algorithm/sort/selection-sort/selection.go
+++ algorithm/sort/selection-sort/selection.go
@@ -16,18 +16,20 @@ func subSelectionSort(data []int, startIndex int) {
 
 }
 
+// 每一轮，只交换一次数据，而冒泡排序，交换次数多
 func evaluationValue(data []int, startIndex int) int {
-	// 临时最小值
-	tempMin := data[startIndex]
+
 	// 临时最小值下标
 	tempMinIndex := startIndex
 
+	// 找到data数据从startIndex后的最小值下标
 	for j := startIndex + 1; j < len(data); j++ {
-		if tempMin > data[j] {
-			tempMinIndex, tempMin = j, data[j]
+		if data[tempMinIndex] > data[j] {
+			tempMinIndex = j
 		}
 	}
 
+	// 将最小值跟data数据中startIndex的元素，进行交换
 	data[startIndex], data[tempMinIndex] = data[tempMinIndex], data[startIndex]
 
 	return startIndex + 1
--- algorithm/sort/selection-sort/selection_test.go
+++ algorithm/sort/selection-sort/selection_test.go
@@ -6,15 +6,13 @@ import (
 )
 
 func TestSelectionSort(b *testing.T) {
-
-
-	//datas := []int{
-	//	9,8,7,6,5,4,3,2,1,
-	//}
-
 	datas := []int{
-		6, 2, 1, 7, 4, 5, 9, 8, 3,
+		9,8,7,6,5,4,3,2,1,
 	}
+	//
+	//datas := []int{
+	//	6, 2, 1, 7, 4, 5, 9, 8, 3,
+	//}
 
 	SelectionSort(datas)
 
