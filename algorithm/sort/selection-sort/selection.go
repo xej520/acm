d2a6fcb1a331dcadd772311289ff7aff2751cab0
故、新 <xingej@163.com>
2020-02-28 15:11:16 +0800

    完成选择排序的第一个版本

--- algorithm/sort/selection-sort/selection.go
+++ algorithm/sort/selection-sort/selection.go
@@ -1,6 +1,34 @@
 package selection_sort
 
+func SelectionSort(data []int) {
+	subSelectionSort(data, 0)
+}
 
+func subSelectionSort(data []int, startIndex int) {
 
+	if startIndex == len(data)-1 {
+		return
+	}
 
+	tempStartIndex := evaluationValue(data, startIndex)
 
+	subSelectionSort(data, tempStartIndex)
+
+}
+
+func evaluationValue(data []int, startIndex int) int {
+	// 临时最小值
+	tempMin := data[startIndex]
+	// 临时最小值下标
+	tempMinIndex := startIndex
+
+	for j := startIndex + 1; j < len(data); j++ {
+		if tempMin > data[j] {
+			tempMinIndex, tempMin = j, data[j]
+		}
+	}
+
+	data[startIndex], data[tempMinIndex] = data[tempMinIndex], data[startIndex]
+
+	return startIndex + 1
+}
--- algorithm/sort/selection-sort/selection_test.go
+++ algorithm/sort/selection-sort/selection_test.go
@@ -1,11 +1,27 @@
 package selection_sort
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+)
 
 func TestSelectionSort(b *testing.T) {
 
 
+	//datas := []int{
+	//	9,8,7,6,5,4,3,2,1,
+	//}
 
+	datas := []int{
+		6, 2, 1, 7, 4, 5, 9, 8, 3,
+	}
+
+	SelectionSort(datas)
+
+	for _, d := range datas{
+		fmt.Printf("%d ", d)
+	}
+	fmt.Println()
 
 }
 
