5312e22ab45876c2a292cee3a472ffbc62d0d311
故、新 <xingej@163.com>
2020-03-01 09:06:46 +0800

    创建希尔排序文件

--- algorithm/sort/insertion-sort/insertion_test.go
+++ algorithm/sort/insertion-sort/insertion_test.go
@@ -6,13 +6,13 @@ import (
 )
 
 func TestInsertSort(b *testing.T) {
-	datas := []int{
-		9,8,7,6,5,4,3,2,1,4,
-	}
-	//
 	//datas := []int{
-	//	6, 2, 1, 7, 4, 5, 9, 8, 3,
+	//	9,8,7,6,5,4,3,2,1,4,
 	//}
+	//
+	datas := []int{
+		6, 2, 1, 7, 4, 5, 9, 8, 3,
+	}
 
 	InsertionSort(datas)
 
