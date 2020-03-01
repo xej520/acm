414f164b1e25421adb8440f2947bf396ad94032a
故、新 <xingej@163.com>
2020-03-01 09:45:14 +0800

    先完成了希尔排序的基本框架，大体流程

--- algorithm/sort/shell-sort/shell.go
+++ algorithm/sort/shell-sort/shell.go
@@ -1,6 +1,27 @@
 package shell_sort
 
-func ShellSort(data []int)  {
+func ShellSort(data []int) {
 
 }
+
+func subShellSort(data []int, interval int) {
+	if interval == 0{
+		return
+	}
+
+	for startIndex := 0; startIndex < len(data); startIndex++ {
+		insertSort(data, startIndex, interval)
+	}
+
+
+	subShellSort(data, interval/2)
+}
+
+func insertSort(data []int, startIndex, interval int) {
+
+}
+
+
+
+
 
